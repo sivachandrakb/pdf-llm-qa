f3f616e720f715b1ac2adf362b14a2fc445c4870
SIVACHANDRA K B <83532038+sivachandrakb@users.noreply.github.com>
2026-03-01 00:24:18 +0530

    Update uncertainty.py

--- uncertainty.py
+++ uncertainty.py
@@ -1,7 +1,7 @@
 import torch
 
-def calculate_entropy(output_scores):
-    probs = torch.nn.functional.softmax(output_scores[-1], dim=-1)
+def calculate_entropy(scores):
+    probs = torch.nn.functional.softmax(scores[-1], dim=-1)
     entropy = -torch.sum(probs * torch.log(probs + 1e-9))
     return entropy.item()
 
