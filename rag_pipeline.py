2849dc0a081c4f57e6f99ea72ff3697ef29025b0
SIVACHANDRA K B <83532038+sivachandrakb@users.noreply.github.com>
2026-03-01 09:57:41 +0530

    Update rag_pipeline.py

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -39,32 +39,28 @@
 #         answer = self.tokenizer.decode(outputs.sequences[0], skip_special_tokens=True)
 #         return answer, outputs
 
-import faiss
 import numpy as np
 from embeddings import get_embeddings
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-import torch
+from sklearn.metrics.pairwise import cosine_similarity
 
 class RAGPipeline:
     def __init__(self):
-        self.index = None
         self.chunks = None
+        self.embeddings = None
 
         self.tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-base")
         self.model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-base")
 
     def build_index(self, texts):
         self.chunks = texts
-        embeddings = get_embeddings(texts)
-        dimension = embeddings.shape[1]
-
-        self.index = faiss.IndexFlatL2(dimension)
-        self.index.add(np.array(embeddings))
+        self.embeddings = get_embeddings(texts)
 
     def retrieve(self, query, k=3):
         query_embedding = get_embeddings([query])
-        distances, indices = self.index.search(np.array(query_embedding), k)
-        return [self.chunks[i] for i in indices[0]]
+        similarities = cosine_similarity(query_embedding, self.embeddings)
+        top_k = similarities.argsort()[0][-k:][::-1]
+        return [self.chunks[i] for i in top_k]
 
     def generate_answer(self, context, question):
         prompt = f"""
